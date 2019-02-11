044912cb046897a686ebcf6647a69455a556dd06
K4P4 <44778275+K4P4@users.noreply.github.com>
2019-02-11 17:21:57 +0200

    Add files via upload

--- Objektinis1.cpp
+++ Objektinis1.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
-using namespace std;
+using std::cin;
+using std::string;
 
-int main(){
-    string vardas, sveikinimas;
-    string eilute1 ="**************", eilute3, eilute2="*            ";
-    int ilgis;
-    char paskutine;
+struct remStrukt{
+int plotis;
+int dydis;
+};
+struct uzrStrukt{
+string vardas;
+string sveikinimas;
+char paskutine;
+int vardIlg;
+};
 
+void sveikinimoGeneravimas(uzrStrukt &uzr) {
     printf("Iveskite savo varda: ");
-    cin >> vardas;
-    ilgis = vardas.length();
-    paskutine = vardas.back();
+    cin >> uzr.vardas;
+    uzr.paskutine = uzr.vardas.back();
+    uzr.vardIlg = uzr.vardas.length();
+    if(uzr.paskutine == 's') {
+        uzr.sveikinimas = "Sveikas, " + uzr.vardas + "!";
+    }
+    else uzr.sveikinimas = "Sveika, " + uzr.vardas + "!";
+}
 
-    if(paskutine == 's') eilute3 = "* Sveikas, " + vardas + "! *";
-    else{
-        eilute3 = "* Sveika, " + vardas + "! *";
-        ilgis--;
+void remelioGeneravimas(uzrStrukt uzr, remStrukt rem) {
+	printf("Iveskite remelio dydi: ");
+	cin >> rem.dydis;
+
+	rem.plotis = rem.dydis * 2 + uzr.vardIlg + 12;
+	if(uzr.paskutine == 's') rem.plotis ++;
+
+	for(int i = 1; i < rem.plotis; i++) printf("*");
+    printf("\n");
+
+    for(int i = 0; i < rem.dydis; i++){
+        printf("%c%*c",'*', rem.plotis-2, '*');
+        printf("\n");
     }
 
-    for(int i = 0; i < ilgis; i++){
-        eilute1 += "*";
-        eilute2 += " ";
+    printf("%*c%s%*c", -(rem.dydis+1),'*', uzr.sveikinimas.c_str() , (rem.dydis+1) ,'*');
+    printf("\n");
+
+    for(int i = 0; i < rem.dydis; i++){
+        printf("%c%*c",'*', rem.plotis-2, '*');
+        printf("\n");
     }
-    eilute2+="*";
+    for(int i = 1; i < rem.plotis; i++) printf("*");
+}
 
-    cout << eilute1 << endl;
-    cout << eilute2 << endl;
-    cout << eilute3 << endl;
-    cout << eilute2 << endl;
-    cout << eilute1 << endl;
 
+int main()
+{
+    remStrukt remelis;
+    uzrStrukt uzrasas;
+
+    sveikinimoGeneravimas(uzrasas);
+    remelioGeneravimas(uzrasas, remelis);
 }
