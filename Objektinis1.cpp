ef955da26fbf78004590f4eca972319d7aad4e9b
K4P4 <44778275+K4P4@users.noreply.github.com>
2019-02-11 14:56:12 +0200

    Add files via upload
    Pridėtas moters vardo atpažinimas

--- Objektinis1.cpp
+++ Objektinis1.cpp
@@ -5,18 +5,26 @@ using namespace std;
 
 int main(){
     string vardas, sveikinimas;
-    string eilute1 ="*************", eilute3, eilute2="*           ";
+    string eilute1 ="**************", eilute3, eilute2="*            ";
     int ilgis;
+    char paskutine;
 
     printf("Iveskite savo varda: ");
     cin >> vardas;
     ilgis = vardas.length();
+    paskutine = vardas.back();
+
+    if(paskutine == 's') eilute3 = "* Sveikas, " + vardas + "! *";
+    else{
+        eilute3 = "* Sveika, " + vardas + "! *";
+        ilgis--;
+    }
+
     for(int i = 0; i < ilgis; i++){
         eilute1 += "*";
         eilute2 += " ";
     }
     eilute2+="*";
-    eilute3 = "* Sveikas " + vardas + "! *";
 
     cout << eilute1 << endl;
     cout << eilute2 << endl;
